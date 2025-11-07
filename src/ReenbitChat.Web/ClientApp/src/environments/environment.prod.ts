9cc952fde81a2e4aa2f4635ca15577a0ace61000
7LOD <Ukhal.v.i@gmail.com>
2025-11-07 14:26:52 +0200

    Update environment for Azure backend

--- src/ReenbitChat.Web/ClientApp/src/environments/environment.prod.ts
+++ src/ReenbitChat.Web/ClientApp/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://reenbitchat-server-c0adandqbxdcczbw.westeurope-01.azurewebsites.net'
+  apiUrl: 'https://reenbitchat-server-c0adandqbxdcczbw.westeurope-01.azurewebsites.net/api',
+  signalrHubUrl: 'https://reenbitchat-server-c0adandqbxdcczbw.westeurope-01.azurewebsites.net/hubs/chat'
 };
 
 console.log('🚀 PROD environment loaded');
