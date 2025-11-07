38f0e332cd401dd38decec72f9469d033a8d3ca6
7LOD <Ukhal.v.i@gmail.com>
2025-11-07 19:52:43 +0200

    🔥 Fixed SignalR endpoint and environment configs

--- src/ReenbitChat.Web/ClientApp/src/environments/environment.ts
+++ src/ReenbitChat.Web/ClientApp/src/environments/environment.ts
@@ -1,6 +1,7 @@
   export const environment = {
     product: false,
-    apiUrl: 'https://localhost:7131'
+    apiUrl: 'https://localhost:7131',
+    signalrHubUrl: 'https://localhost:7131'
   };
 
 console.log('💻 DEV environment loaded');
