ba5723f2251d216014f322786fa109a57d154605
7LOD <Ukhal.v.i@gmail.com>
2025-11-07 21:36:21 +0200

    Debag

--- src/ReenbitChat.Web/ClientApp/src/environments/environment.prod.ts
+++ src/ReenbitChat.Web/ClientApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://reenbitchat-server-c0adandqbxdcczbw.westeurope-01.azurewebsites.net/api',
+  apiUrl: 'https://reenbitchat-server-c0adandqbxdcczbw.westeurope-01.azurewebsites.net',
   signalrHubUrl: 'https://reenbitchat-server-c0adandqbxdcczbw.westeurope-01.azurewebsites.net/hubs/chat'
 };
 console.log('🚀 PROD environment loaded');
